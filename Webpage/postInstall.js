c39a4bd307198e81468de86b63a8f219781f2a65
maurinElectroTextile <maurin@datapaulette.org>
2018-05-02 10:59:55 +0200

    Removed Jscad

--- Webpage/postInstall.js
+++
@@ -1,5 +0,0 @@
-// this copies the examples folder from the @jscad/examples repository locally
-const path = require('path')
-const examplesPath = path.resolve('./node_modules/@jscad/examples')
-const copydir = require('copy-dir')
-copydir.sync(examplesPath, 'examples')
