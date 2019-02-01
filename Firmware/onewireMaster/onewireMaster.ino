769294f4733e1caf86e66f8dacc5f4f44057887e
maurinElectroTextile <maurin@datapaulette.org>
2019-02-01 12:34:00 +0100

    CleanUP

--- Firmware/onewireMaster/onewireMaster.ino
+++ Firmware/onewireMaster/onewireMaster.ino
@@ -1,22 +1,25 @@
+/*
+   Lulu_1-Wire
+   This file is part of Lulu-MCU project: http://lulu.eTextile.org
+*/
+
 #include <OneWire.h>
 
 OneWire net(10);  // on pin 10
 
 const int LED_PIN = 13;
-const int ONEWIRE_PIN = 10;
 
 void setup(void) {
-  Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
-  pinMode(ONEWIRE_PIN, OUTPUT);
 }
 
 void loop(void) {
+
+  delay(1000);
   digitalWrite(LED_PIN, LOW);
-  net.reset();
-  net.write_bytes(0xF0, 1);
-  Serial.println("WRITE_BYTE");
   delay(1000);
   digitalWrite(LED_PIN, HIGH);
-  delay(1000);
+  net.reset();
+  net.write_bytes(0xFF, 1); // 255 // 1111 1111
+
 }
